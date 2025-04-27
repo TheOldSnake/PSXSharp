74709304e8940310598a22d373d6815a2c147ec1
TheOldSnake <st1107082@gmail.com>
2025-04-27 20:37:12 +0300

    Comment

--- PSXSharp/Other/CACHECONTROL.cs
+++ PSXSharp/Other/CACHECONTROL.cs
@@ -7,6 +7,7 @@ namespace PSXSharp {
 
         public Range Range = new Range(0xfffe0130, 4);      
         public void WriteWord(uint address, uint value) {
+            //Invalidate all ram blocks when this register is written
             CPUWrapper.GetCPUInstance().SetInvalidAllRAMBlocks();
         }
     }
